5a7af2b745e1e68d4fcdd9075e372835af51b91e
Jean Jacques <jjpoirier@gmail.com>
2023-10-09 11:18:11 +0200

    ternaire1

--- 02_ternaire.js
+++ 02_ternaire.js
@@ -6,6 +6,9 @@
  *    - utiliser l'opérateur ternaire (if interdit)
  */
 function ternaire(a , b) {
+    return ((a > b) ? a : b);
 }
+
+console.log(ternaire(11, 54));
 
 module.exports = ternaire;
