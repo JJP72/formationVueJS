9036c8b4a1c8d2821352e86881c202b5df141c47
Jean Jacques <jjpoirier@gmail.com>
2023-10-09 17:32:02 +0200

    promise1

--- 10_promise.js
+++ 10_promise.js
@@ -6,7 +6,27 @@
  */
 
 const sleep = () => {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("set");
+            resolve(42+1); // ok
+            resolve(-42);// Erreur
+        }, 5000);
+    });
 };
+
+console.log("debut");
+//sleep().then((res) => ...); .....////
+console.log("fin");
+
+const asyncFonction = async() => {
+
+}
+
+
+console.log("debut");
+
+console.log("fin");
+
 
 module.exports = {sleep};
